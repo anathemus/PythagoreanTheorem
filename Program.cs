6ccf0be9c12c4b337691cdc02df475c4b2d3e90f
anathemus <anathemus@gmail.com>
2015-09-08 10:19:28 -0400

    Pythagorean Theorem
    Applies Pythagorean Theorem to 3 digits to tell if triangle is Right,
    Acute, or Obtuse.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PythagoreanTheorem
+{
+    class Pythagorean 
+    {
+        public int a, b, c;
+        public String[] coversion;
+
+        public Pythagorean(String[] conversion) 
+        {
+            a = Convert.ToInt32(conversion[0]);
+            b = Convert.ToInt32(conversion[1]);
+            c = Convert.ToInt32(conversion[2]);
+            if ((a*a)+(b*b) > (c*c))
+            {
+                Console.WriteLine("Acute");
+            }
+            else if ((a*a)+(b*b) == (c*c))
+            {
+                Console.WriteLine("Right");
+            }
+            else 
+            {
+                Console.WriteLine("Obtuse");
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String elements = Console.ReadLine();
+            String[] split_elements = elements.Split(' ');
+            
+            Pythagorean triangle = new Pythagorean(split_elements);
+
+            Console.ReadLine();
+        }
+    }
+}
